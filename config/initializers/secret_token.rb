e6705e28f1a4e28588ef00e6d934bcca59b1894a
your-name <Dina.abandah@gmail.com>
2014-12-23 00:37:43 +0200

    first commit s with '#' will be ignored, and an empty message aborts the commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '4ade2339dc88869d4c243143894d7ffe6c5b7499c987ed54feb45fe4c48de168289a15a0025f50c57271df815b8cfdcea320c993d755dd27b06bc793249c6523'
